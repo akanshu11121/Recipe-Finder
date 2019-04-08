5fab46e49cc6150f60fb033cadd5ba812c42093e
Akanshu Sonkar <akanshu11121@gmail.com>
2019-04-08 16:05:43 +0530

    Add files via upload

---
+++ build/precache-manifest.f57a5cad340143f03941117ed338f824.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "d820b497cc152184d0f6620a9ec15714",
+    "url": "/static/media/bg.d820b497.jpeg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "7e8385bcfd8927477662",
+    "url": "/static/js/main.9ef42310.chunk.js"
+  },
+  {
+    "revision": "776e000f7ab63d5becb6",
+    "url": "/static/js/2.17628d53.chunk.js"
+  },
+  {
+    "revision": "7e8385bcfd8927477662",
+    "url": "/static/css/main.466fa956.chunk.css"
+  },
+  {
+    "revision": "8453889d2e9b1eca36da47bde5b00217",
+    "url": "/index.html"
+  }
+];
